ed7dd7908542da9637d54e400fdb1f2cac053666
Colin Brown <kolivbrauv@gmail.com>
2017-11-02 16:28:18 -0700

    Created interpreter to launch when no argv given

--- ui.py
+++ ui.py
@@ -9,7 +9,8 @@ from data_report import DataReport
 ################################################################################
 # Globals
 
-PROGRAM_NAME = 'hwhack'
+PROGRAM_NAME = 'Homework Hacker'
+PROGRAM_CMD = 'hwhack'
 VERSION = 'Dev'
 
 COMMAND_LIST = ['database', 'edit', 'help', 'list', 'new']
@@ -21,6 +22,9 @@ COMMAND_DESCRIPTION = {
     'new': 'Create a new Assignment'
 }
 
+PROMPT = '\U0001F4DA  $ '
+INDENT = ' ' * 4
+
 
 ################################################################################
 # Generic Functions
@@ -28,17 +32,30 @@ COMMAND_DESCRIPTION = {
 def safe_input(prompt: str=''):
     try:
         return input(prompt)
-    except EOFError: # User aborted
-        sys.exit()
+    except (KeyboardInterrupt, EOFError):  # User aborted
+        print()
+        sys.exit(0)
 
 
 ################################################################################
 # Primary Functions
 
-def print_greeting():
-    title = 'Homework Hacker (v. {})'.format(VERSION)
+def print_help(synopsis: bool, end: str = ''):
+    result = ''
+    if synopsis:
+        result += 'SYNOPSIS:\n    {} [command]\n\n'.format(PROGRAM_CMD)
+    result += 'COMMANDS:\n'
+    for cmd in COMMAND_LIST:
+        result += '    {} - {}\n'.format(cmd, COMMAND_DESCRIPTION[cmd])
+    result = result[:-1]  # strip last newline
+    result += end
+    print(result)
+
+
+def print_greeting(synopsis: bool = True, end: str = ''):
+    title = '{} (v. {})'.format(PROGRAM_NAME, VERSION)
     print('{}\n{}'.format(title, '~' * len(title)))
-    print_help()
+    print_help(synopsis, end)
 
 
 def check_database(verbose: bool) -> (dict, bool):
@@ -65,13 +82,6 @@ def prompt_edit():
     pass
 
 
-def print_help():
-    print('SYNOPSIS:\n    {} [command]\n'.format(PROGRAM_NAME))
-    print('COMMANDS:')
-    for cmd in COMMAND_LIST:
-        print('    {} - {}'.format(cmd, COMMAND_DESCRIPTION[cmd]))
-
-
 def print_list():
     db_data = database.load()
     print(DataReport(
@@ -80,13 +90,13 @@ def print_list():
 
 
 def prompt_new():
-    db_data, prompted = check_database(verbose=False)
+    db_data, prompted = check_database(verbose = False)
     if db_data:
         if prompted:
             print('')
         print('NEW ASSIGNMENT:')
         # Prompt title
-        title = safe_input('    Title: ')
+        title = safe_input(INDENT + 'Title: ')
 
         # Prompt course
         course_set = set()
@@ -94,12 +104,12 @@ def prompt_new():
             course_set.add(assignment['course'])
         course_list = sorted(course_set)
 
-        print('    Course', end='')
+        print(INDENT + 'Course', end='')
         for i, course in enumerate(course_list):
             if i == 0:
                 print('')
-            print('        [{}] {}'.format(i+1, course))
-        course = safe_input('{}: '.format('    ' if course_list else ''))
+            print('{}[{}] {}'.format(INDENT * 2, i+1, course))
+        course = safe_input('{}: '.format(INDENT if course_list else ''))
         globbed = False
         try:
             index = int(course)
@@ -110,12 +120,12 @@ def prompt_new():
             pass
 
         # Prompt due date
-        due_date = safe_input('    Due Date: ')
+        due_date = safe_input(INDENT + 'Due Date: ')
 
         # Confirm
         if globbed and db_data['settings']['confirmOnGlob']:
-            print('\nCONFIRM:\n    Title: {}\n    Course: {}\n    Due Date: {}\n'
-                  .format(title, course, due_date))
+            print('\nCONFIRM:\n{0}Title: {1}\n{0}Course: {2}\n{0}Due Date: {3}\n'
+                  .format(INDENT, title, course, due_date))
             if safe_input('(y/n) ').lower() != 'y':
                 return
 
@@ -125,24 +135,24 @@ def prompt_new():
         database.save(db_data)
 
 
-def print_unknown(cmd):
+def print_unknown(cmd: str):
     print('{}: Unknown command: '.format(PROGRAM_NAME), end='')
     if type(cmd) == type(""): # str
-        print('{}'.format(PROGRAM_NAME, cmd))
+        print('{}'.format(cmd))
     elif type(cmd) == type([]): # list
         print(*cmd, sep=' ')
 
 
-def parse_args(argv: list):
+def parse_args(argv: [str]):
     if not argv:
-        print_greeting()
+        launch_interactive()
     elif len(argv) == 1:
         if argv[0] == 'database':
             check_database(verbose=True)
         elif argv[0] == 'edit':
             prompt_edit()
         elif argv[0] == 'help':
-            print_help()
+            print_greeting()
         elif argv[0] == 'list':
             print_list()
         elif argv[0] == 'new':
@@ -151,6 +161,14 @@ def parse_args(argv: list):
             print_unknown(argv[0])
     else:
         print_unknown(argv)
+
+
+def launch_interactive():
+    print_greeting(False)
+
+    while True:
+        user_input = safe_input('\n' + PROMPT)
+        parse_args(user_input.split())
 
 
 ################################################################################
